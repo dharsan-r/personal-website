78885ea3a26b8d4ece0057769713569a08db9321
Dharsan R <dharsan.ravi07@gmail.com>
2022-01-01 01:50:46 -0500

    added projects page, hover effects

--- mobile.js
+++ mobile.js
@@ -10,7 +10,7 @@ const navSlide = () => {
             if(link.style.animation){
                 link.style.animation='';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.3}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index /5 + 0.2}s`;
             }
         })
 
