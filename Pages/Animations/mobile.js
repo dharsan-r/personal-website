bfa6e77f5237bb9caefcd7289b80e820f41ff7cf
Dharsan Ravindran <67665008+dharsan-r@users.noreply.github.com>
2022-09-13 22:43:22 -0400

    major feature changes

--- Pages/Animations/mobile.js
+++ Pages/Animations/mobile.js
@@ -10,7 +10,7 @@ const navSlide = () => {
             if(link.style.animation){
                 link.style.animation='';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index /5 + 0.05}s`;
+                link.style.animation = `navLinkFade 0.27s ease forwards ${index /5 + 0.05}s`;
             }
         })
 
